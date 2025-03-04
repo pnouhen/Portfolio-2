b68e08baa294ef412c6a41ef0cd8f7531486345c
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-03-04 10:06:40 +0100

    Modification pour avoir le fetch avec Vercel

--- src/components/service/GenerateData.jsx
+++ src/components/service/GenerateData.jsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react";
-export default function GenerateData({setData, link}){
-    useEffect(() => {
-        fetch(link)
-          .then((response) => response.json())
-          .then((data) => {
-              setData(data);
-          })
-          .catch((error) =>
-            console.error("Erreur lors de la récupération des données :", error)
-          );
-          // Table according to the elements to be modified
-      }, [setData, link]); 
-      return null;     
+
+export default function GenerateData({ setData, link }) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(link);        
+        if (!response.ok) {
+          throw new Error(`Erreur réseau: ${response.status}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des données :", error);
+      }
+    };
+    fetchData();
+  }, [link, setData]);
+  return null;
 }
