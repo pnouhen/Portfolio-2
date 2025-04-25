ae9a38160217522e3bacd29e00de43b7c5093103
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-25 16:12:51 +0200

    Amélioration de performance via Vercel 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
-
-// Code pour supprimer l'élément h1 de Vercel
-const heading = document.querySelector('h1[data-version="v1"].text_wrapper__i87JK');
-if (heading) {
-  heading.remove();
-}
+)
